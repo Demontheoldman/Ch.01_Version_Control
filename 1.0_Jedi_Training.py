bc5a6ab8570f9aa29ccb66014190202607dd6378
Demontheoldman <ahmedare@urbandaleschools.com>
2024-08-29 15:08:31 -0500

    Update 1.0_Jedi_Training.py

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -26,27 +26,70 @@ The last line keeps the window open until you click to close.
 Turtle Documentation: https://docs.python.org/3.3/library/turtle.html?highlight=turtle
 '''
 import turtle
-yoda=turtle.Turtle()
-screen=turtle.Screen() # makes a screen object
-screen.bgcolor('black') # colors the screen
-yoda.pensize(3) # width of pen line
-yoda.speed(10)  # speed of drawing. Go fast to not waste time.
-yoda.color("#00FF00")
-yoda.circle(100)  #head
-yoda.penup()
-yoda.setpos(50,185) #right ear
-yoda.pendown()
-yoda.goto(200,210)
-yoda.goto(88,145)
-yoda.penup()
-yoda.setpos(-50,185)  #left ear
-yoda.pendown()
-yoda.goto(-200,210)
-yoda.goto(-88,145)
-yoda.penup()
-yoda.setpos(200,-300)
-yoda.pendown()
-yoda.pencolor('#00FF00')
+
+tina = turtle.Turtle()
+tommy = turtle.Turtle()
+
+tina.color('blue')
+tommy.color('green')
+
+tina.shape('turtle')
+tommy.shape('turtle')
+
+tina.speed(50)
+tommy.speed(50)
+
+tommy.pendown()
+tommy.begin_fill()
+tommy.color('cyan')
+tommy.goto(200,200)
+tommy.pendown()
+tommy.goto(200,-200)
+tommy.goto(-200,-200)
+tommy.goto(-200,200)
+tommy.goto(200,200)
+tommy.penup()
+tommy.end_fill()
+
+#tina make the circle for the flag
+
+tina.penup()
+tina.begin_fill()
+tina.color('red')
+tina.goto(0,0)
+tina.pendown()
+tina.circle(50)
+tina.penup()
+tina.end_fill()
+
+#tommy is making the square for the flag
+
+tommy.penup()
+
+tommy.color('green')
+tommy.goto(-100,120)
+tommy.begin_fill()
+tommy.pendown()
+tommy.forward(200)
+tommy.right(90)
+tommy.forward(150)
+tommy.right(90)
+tommy.forward(200)
+tommy.right(90)
+tommy.forward(150)
+tommy.end_fill()
+
+#tommy makes the white line for the flag 
+
+tommy.left(90)
+tommy.forward(10)
+tommy.left(90)
+tommy.forward(300)
+tommy.left(90)
+
+
+
+
 
 
 yoda.write('Sign Your Name Here',font=("Arial", 16, "normal")) # signs your name to your art
